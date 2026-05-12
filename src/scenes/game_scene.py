dc5994a9d077bdf755c693d3bc3bbc9398ba74a3
Alrykemes <alrykemesgc@gmail.com>
2026-05-11 22:50:28 -0300

    fix: update game method to animate spritesheets

--- src/scenes/game_scene.py
+++ src/scenes/game_scene.py
@@ -2,6 +2,7 @@ import pygame
 
 from core.window import Window
 from core.settings import Settings
+from game_objects.airplane import Airplane
 
 class GameScene:
     def __init__(self, screen):
@@ -21,11 +22,21 @@ class GameScene:
             self.objectGroup.add(self.bg)
         except FileNotFoundError:
             print("Background 'background-day.png' não encontrado!")
+        
+        # ====================== AIRPLANE ======================
+        self.airplane = pygame.sprite.Sprite()
+        airplane = Airplane((200, 200))
+        self.objectGroup.add(airplane)
+        
+
+    def update(self):
+        
+        self.objectGroup.update()
 
     def draw(self):
         """Desenha tudo na tela"""
         self.objectGroup.draw(self.window.get_surface())
-        self.window.update()  # pygame.display.update()
+        self.window.update()
 
     def handle_event(self, event):
 
