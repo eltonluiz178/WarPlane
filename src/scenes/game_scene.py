78065b1ed302f3b1b7e604d30a58a32854366f62
eltonluiz178 <eltonluiz533@gmail.com>
2026-05-18 18:47:45 -0300

    feat: usa lógica do botão dinâmico em todo projeto

--- src/scenes/game_scene.py
+++ src/scenes/game_scene.py
@@ -3,6 +3,8 @@ import pygame
 from core.window import Window
 from core.settings import Settings
 from game_objects.airplane import Airplane
+from utils.game_button import GameButton
+
 
 class GameScene:
     def __init__(self, screen):
@@ -10,12 +12,9 @@ class GameScene:
         self.settings = Settings()
         self.window = Window(self.settings)
 
-        self.pause_button = pygame.Rect(self.settings.WIDTH - 120, 20, 100, 50)
+        self.pause_button_rect = pygame.Rect(self.settings.WIDTH - 120, 20, 100, 50)
 
-        self.font = pygame.font.Font(
-            "assets/fonts/press_start_regular.ttf",
-            12
-        )
+        self.pause_button = GameButton(self.screen, "PAUSE", self.pause_button_rect, 0)
 
         # ====================== GRUPOS DE SPRITES ======================
         self.objectGroup = pygame.sprite.Group()
@@ -43,7 +42,7 @@ class GameScene:
     def draw(self):
         """Desenha tudo na tela"""
         self.objectGroup.draw(self.window.get_surface())
-        self.draw_button("PAUSE", self.pause_button)
+        self.pause_button.draw()
         self.window.update()
 
 
@@ -55,70 +54,7 @@ class GameScene:
 
                 mouse_pos = pygame.mouse.get_pos()
 
-                if self.pause_button.collidepoint(mouse_pos):
+                if self.pause_button_rect.collidepoint(mouse_pos):
                     return "pause"
 
         return None
-
-
-    def draw_button(self, text, rect, selected=False):
-        x = rect.x
-        y = rect.y
-
-        mouse_pos = pygame.mouse.get_pos()
-
-        hovered = rect.collidepoint(mouse_pos)
-
-        active = hovered or selected
-
-        # Cores
-        if active:
-            bg_color = (168, 78, 18)
-            border_color = (255, 170, 70)
-            text_color = (255, 245, 220)
-            arrow_color = (255, 140, 0)
-        else:
-            bg_color = (18, 18, 18)
-            border_color = (90, 90, 50)
-            text_color = (230, 230, 230)
-
-        # Fundo principal
-        pygame.draw.rect(
-            self.screen,
-            bg_color,
-            rect,
-            border_radius=4
-        )
-
-        # Borda externa
-        pygame.draw.rect(
-            self.screen,
-            border_color,
-            rect,
-            width=2,
-            border_radius=4
-        )
-
-        # Linha interna
-        inner_rect = rect.inflate(-8, -8)
-
-        pygame.draw.rect(
-            self.screen,
-            (60, 60, 40),
-            inner_rect,
-            width=1,
-            border_radius=2
-        )
-
-        # Texto
-        label = self.font.render(
-            text,
-            True,
-            text_color
-        )
-
-        text_rect = label.get_rect(
-            center=rect.center
-        )
-
-        self.screen.blit(label, text_rect)
--- src/scenes/menu_scene.py
+++ src/scenes/menu_scene.py
@@ -1,5 +1,8 @@
 import pygame
 
+from utils.game_button import GameButton
+
+
 class MenuScene:
     def __init__(self, screen):
         self.screen = screen
@@ -35,11 +38,13 @@ class MenuScene:
             12
         )
 
-        self.play_button = pygame.Rect(230, 240, 260, 58)
+        self.play_button_rect = pygame.Rect(230, 240, 260, 58)
+        self.config_button_rect = pygame.Rect(230, 310, 260, 58)
+        self.extras_button_rect = pygame.Rect(230, 380, 260, 58)
 
-        self.config_button = pygame.Rect(230, 310, 260, 58)
-
-        self.extras_button = pygame.Rect(230, 380, 260, 58)
+        self.play_button = GameButton(self.screen, "JOGAR", self.play_button_rect, 1)
+        self.config_button = GameButton(self.screen, "CONFIGURAÇÕES", self.config_button_rect, 1)
+        self.extras_button = GameButton(self.screen, "EXTRAS", self.extras_button_rect, 1)
 
     def handle_event(self, event):
 
@@ -49,7 +54,7 @@ class MenuScene:
 
                 mouse_pos = pygame.mouse.get_pos()
 
-                if self.play_button.collidepoint(mouse_pos):
+                if self.play_button.rect.collidepoint(mouse_pos):
                     return "game"
 
         return None
@@ -64,83 +69,6 @@ class MenuScene:
 
         self.screen.blit(self.background, (bg_x, bg_y))
 
-
-        self.draw_button("JOGAR", self.play_button)
-        self.draw_button("CONFIGURAÇÕES", self.config_button)
-        self.draw_button("EXTRAS", self.extras_button)
-
-    def draw_button(self, text, rect, selected=False):
-        x = rect.x
-        y = rect.y
-
-        mouse_pos = pygame.mouse.get_pos()
-
-        hovered = rect.collidepoint(mouse_pos)
-
-        active = hovered or selected
-
-        # Cores
-        if active:
-            bg_color = (168, 78, 18)
-            border_color = (255, 170, 70)
-            text_color = (255, 245, 220)
-            arrow_color = (255, 140, 0)
-        else:
-            bg_color = (18, 18, 18)
-            border_color = (90, 90, 50)
-            text_color = (230, 230, 230)
-
-        # Fundo principal
-        pygame.draw.rect(
-            self.screen,
-            bg_color,
-            rect,
-            border_radius=4
-        )
-
-        # Borda externa
-        pygame.draw.rect(
-            self.screen,
-            border_color,
-            rect,
-            width=2,
-            border_radius=4
-        )
-
-        # Linha interna
-        inner_rect = rect.inflate(-8, -8)
-
-        pygame.draw.rect(
-            self.screen,
-            (60, 60, 40),
-            inner_rect,
-            width=1,
-            border_radius=2
-        )
-
-        # Seta apenas no hover
-        if active:
-            points = [
-                (x + 14, y + 29),
-                (x + 24, y + 22),
-                (x + 24, y + 36)
-            ]
-
-            pygame.draw.polygon(
-                self.screen,
-                arrow_color,
-                points
-            )
-
-        # Texto
-        label = self.font.render(
-            text,
-            True,
-            text_color
-        )
-
-        text_rect = label.get_rect(
-            center=rect.center
-        )
-
-        self.screen.blit(label, text_rect)
+        self.play_button.draw()
+        self.config_button.draw()
+        self.extras_button.draw()
