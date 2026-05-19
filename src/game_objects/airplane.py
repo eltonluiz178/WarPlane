a89dd7d4b4194d566e51543ae64b476920b6b68a
Alrykemes <alrykemesgc@gmail.com>
2026-05-18 21:11:44 -0300

    feat: airplane moviments

--- src/game_objects/airplane.py
+++ src/game_objects/airplane.py
@@ -8,11 +8,10 @@ class Airplane(pygame.sprite.Sprite):
         self.current_frame = 0
         self.animation_speed = 0.45
 
-        # Configuração dos frames
+        # ================= ANIMAÇÃO =================
         self.frame_width = 200
         self.frame_height = 100
 
-        # Carrega spritesheet
         sprite_sheet = pygame.image.load(
             "./assets/images/sprites/airplane_sprite.png"
         ).convert_alpha()
@@ -21,7 +20,6 @@ class Airplane(pygame.sprite.Sprite):
 
         total_frames = sheet_width // self.frame_width
 
-        # Corta os frames
         for i in range(total_frames):
 
             frame = sprite_sheet.subsurface(
@@ -38,7 +36,43 @@ class Airplane(pygame.sprite.Sprite):
         self.image = self.frames[0]
         self.rect = self.image.get_rect(topleft=pos)
 
-    def update(self):
+        # ================= MOVIMENTO =================
+        self.speed = 5
+
+    def movement(self):
+
+        keys = pygame.key.get_pressed()
+
+        # Esquerda
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+
+        # Direita
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+
+        # Cima
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+
+        # Baixo
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+            
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if self.rect.right > 1280:
+            self.rect.right = 1280
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+        if self.rect.bottom > 720:
+            self.rect.bottom = 720
+
+    def animate(self):
+
         self.current_frame += self.animation_speed
 
         if self.current_frame >= len(self.frames):
@@ -46,9 +80,7 @@ class Airplane(pygame.sprite.Sprite):
 
         self.image = self.frames[int(self.current_frame)]
 
-    # Para utilizar:
-    # # Cria objeto animado
-    # airplane = Airplane((200, 200))
-    # self.objectGroup.add(airplane)
-    # # # Adiciona no grupo
-    # self.objectGroup.add(airplane)
+    def update(self):
+
+        self.movement()
+        self.animate()
